7b1731f726efb9010ad5e1f9b58081840a137766
Sakshi Patil <123533124+SakshiPatil6@users.noreply.github.com>
2023-10-31 06:37:13 +0530

    Add files via upload

---
+++ practical1/p1.py
@@ -0,0 +1,11 @@
+print("Hello")
+val1=input("Enter value 1: ")
+val2=input("Enter value 2: ")
+intval1=int(val1)
+intval2=int(val2)
+result= intval1+intval2
+print("Type of val1: ",type(val1))
+print("Type of intval1: ",type(intval1))
+print("Result:",result)
+#print("Result: ",val1+val2)
+
